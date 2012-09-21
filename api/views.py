5d454742dcc452f762db5574cef479ce9ddd9dea
Michael McLarnon <mmclar@gmail.com>
2012-09-21 09:45:38 -0400

    Initial api handlers.

--- api/views.py
+++ api/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from piston.handler import BaseHandler
+from todo.api import List, Item
+
+class ListHandler(BaseHandler):
+    model = List
+    allowed_methods = ('PUT', 'DELETE',)
