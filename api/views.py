c32c7213b94663f359f10dfbf2cce8c439e18721
Michael McLarnon <michael@redrawnlabs.com>
2012-09-22 11:29:00 -0400

    Be able to query items beased on added or due dates.

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from piston.handler import BaseHandler
 from api.models import List, Item
 import simplejson
+from datetime import date
 from piston.utils import Mimer
 Mimer.register(simplejson.loads, ('application/json', 'application/json; charset=UTF-8',))
 
@@ -8,6 +9,24 @@ class ListHandler(BaseHandler):
     model = List
     allowed_methods = ('POST', 'PUT', 'DELETE',)
 
+def date_from_param(request, param):
+    d = request.GET.get(param, None)
+    if d:
+        parts = d.split('-')
+        d = date(int(parts[0]), int(parts[1]), int(parts[2]))
+    return d
+
 class ItemHandler(BaseHandler):
     model = Item
-    allowed_methods = ('POST', 'PUT', 'DELETE',)
+    allowed_methods = ('POST', 'PUT', 'DELETE', 'GET',)
+
+    def read(self, request):
+        due_date = date_from_param(request, 'dueDate')
+        added_date = date_from_param(request, 'addedDate')
+        matches = Item.objects.all()
+        if due_date:
+            matches = matches.filter(dueDate=due_date)
+        if added_date:
+            matches = matches.filter(addedDate=added_date)
+        return matches
+
